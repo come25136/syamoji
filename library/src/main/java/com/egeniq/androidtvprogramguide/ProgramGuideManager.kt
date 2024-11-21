e764b28623fd708ba019fe6235313f85cf90d045
come25136 <git@come25136.id>
2024-11-22 00:58:33 +0900

    fix: 0~4時に番組表を開くと番組見れない

--- library/src/main/java/com/egeniq/androidtvprogramguide/ProgramGuideManager.kt
+++ library/src/main/java/com/egeniq/androidtvprogramguide/ProgramGuideManager.kt
@@ -37,7 +37,7 @@ class ProgramGuideManager<T> {
         internal val ENTRY_MIN_DURATION = TimeUnit.MINUTES.toMillis(2) // 2 min
         private val MAX_UNACCOUNTED_TIME_BEFORE_GAP = TimeUnit.MINUTES.toMillis(15) // 15 min
 
-        private const val DAY_STARTS_AT_HOUR = 4
+        private const val DAY_STARTS_AT_HOUR = 0
         private const val DAY_ENDS_NEXT_DAY_AT_HOUR = 4
 
         private val TAG: String = ProgramGuideManager::class.java.name
